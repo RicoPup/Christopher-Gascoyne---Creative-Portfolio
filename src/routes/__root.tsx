463c5918abf044e91960573863fe189ad6c1adc7
RicoPup <rico-dawg@hotmail.co.uk>
2026-06-12 23:22:51 -0400

    analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Analytics } from '@vercel/analytics/react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
@@ -63,6 +64,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
             },
           ]}
         />
+        <Analytics />
         <Scripts />
       </body>
     </html>
